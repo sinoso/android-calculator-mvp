c9c6c8f08cdd85a384b073e3cfa5c3e81c565fae
ganghee <35513039+ganghee@users.noreply.github.com>
2022-01-27 23:22:26 +0900

    Step1 모듈분리 (#16)
    * feat: domain모듈 추가
    
    * feat: 1주 차에서 모듈화한 코드 구현하기
    
    * feat: build.gradle(app)에 domain 모듈화 의존성 추가
    
    * docs: readme - MVP step1 요구사항
    
    * refact: package명 수정

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -4,88 +4,61 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
-import edu.nextstep.camp.calculator.domain.Calculator
-import edu.nextstep.camp.calculator.domain.Expression
-import edu.nextstep.camp.calculator.domain.Operator
+import edu.nextstep.camp.domain.Expression
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val calculator = Calculator()
-    private var expression = Expression.EMPTY
+    private val expression by lazy { Expression() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button0.setOnClickListener {
-            expression += 0
-            binding.textView.text = expression.toString()
+        clickButtonListener()
+    }
+
+    private fun clickButtonListener() {
+        binding.button0.setOnClickListener { appendOperand(getString(R.string.calculator_0)) }
+        binding.button1.setOnClickListener { appendOperand(getString(R.string.calculator_1)) }
+        binding.button2.setOnClickListener { appendOperand(getString(R.string.calculator_2)) }
+        binding.button3.setOnClickListener { appendOperand(getString(R.string.calculator_3)) }
+        binding.button4.setOnClickListener { appendOperand(getString(R.string.calculator_4)) }
+        binding.button5.setOnClickListener { appendOperand(getString(R.string.calculator_5)) }
+        binding.button6.setOnClickListener { appendOperand(getString(R.string.calculator_6)) }
+        binding.button7.setOnClickListener { appendOperand(getString(R.string.calculator_7)) }
+        binding.button8.setOnClickListener { appendOperand(getString(R.string.calculator_8)) }
+        binding.button9.setOnClickListener { appendOperand(getString(R.string.calculator_9)) }
+
+        binding.buttonPlus.setOnClickListener { appendOperator(getString(R.string.calculator_plus)) }
+        binding.buttonMinus.setOnClickListener { appendOperator(getString(R.string.calculator_minus)) }
+        binding.buttonDivide.setOnClickListener { appendOperator(getString(R.string.calculator_divide)) }
+        binding.buttonMultiply.setOnClickListener { appendOperator(getString(R.string.calculator_multiply)) }
+        binding.buttonDelete.setOnClickListener { deleteLastElement() }
+        binding.buttonEquals.setOnClickListener { calculateStatement() }
+    }
+
+    private fun appendOperand(operand: String) {
+        binding.textView.text =
+            expression.appendStatement(binding.textView.text.toString(), operand)
+    }
+
+    private fun appendOperator(operator: String) {
+        if (binding.textView.text.isNotEmpty()) {
+            binding.textView.text =
+                expression.appendStatement(binding.textView.text.toString(), operator)
         }
-        binding.button1.setOnClickListener {
-            expression += 1
-            binding.textView.text = expression.toString()
-        }
-        binding.button2.setOnClickListener {
-            expression += 2
-            binding.textView.text = expression.toString()
-        }
-        binding.button3.setOnClickListener {
-            expression += 3
-            binding.textView.text = expression.toString()
-        }
-        binding.button4.setOnClickListener {
-            expression += 4
-            binding.textView.text = expression.toString()
-        }
-        binding.button5.setOnClickListener {
-            expression += 5
-            binding.textView.text = expression.toString()
-        }
-        binding.button6.setOnClickListener {
-            expression += 6
-            binding.textView.text = expression.toString()
-        }
-        binding.button7.setOnClickListener {
-            expression += 7
-            binding.textView.text = expression.toString()
-        }
-        binding.button8.setOnClickListener {
-            expression += 8
-            binding.textView.text = expression.toString()
-        }
-        binding.button9.setOnClickListener {
-            expression += 9
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonPlus.setOnClickListener {
-            expression += Operator.Plus
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonMinus.setOnClickListener {
-            expression += Operator.Minus
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonMultiply.setOnClickListener {
-            expression += Operator.Multiply
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonDivide.setOnClickListener {
-            expression += Operator.Divide
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonDelete.setOnClickListener {
-            expression = expression.removeLast()
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonEquals.setOnClickListener {
-            val result = calculator.calculate(expression.toString())
-            if (result == null) {
-                Toast.makeText(this, R.string.incomplete_expression, Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            expression = Expression.EMPTY + result
-            binding.textView.text = result.toString()
+    }
+
+    private fun deleteLastElement() {
+        binding.textView.text = expression.deleteLastElement(binding.textView.text.toString())
+    }
+
+    private fun calculateStatement() {
+        runCatching {
+            binding.textView.text = expression.calculatedValue(binding.textView.text.toString())
+        }.onFailure { e ->
+            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
         }
     }
 }
