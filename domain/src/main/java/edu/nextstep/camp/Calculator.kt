368a731756da5290e77957b64997b47dfbe8b6bd
Woogear Kwon <woogear.kwon@huray.net>
2022-01-27 23:08:16 +0900

    [Feat] 도메인 모듈 분리 (#18)

--- domain/src/main/java/edu/nextstep/camp/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/Calculator.kt
@@ -1,4 +1,4 @@
-package edu.nextstep.camp.calculator.domain
+package edu.nextstep.camp
 
 class Calculator {
     fun calculate(rawExpression: String): Int? {
--- domain/src/test/kotlin/edu/nextstep/camp/CalculatorTest.kt
+++ domain/src/test/kotlin/edu/nextstep/camp/CalculatorTest.kt
@@ -1,4 +1,4 @@
-package edu.nextstep.camp.calculator.domain
+package edu.nextstep.camp
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.BeforeEach
