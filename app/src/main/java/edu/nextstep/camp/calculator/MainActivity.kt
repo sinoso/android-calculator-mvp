373c7f2428788dd2535efb529bfbb60486299bf6
박찬준 <31771548+ChanJun-Park@users.noreply.github.com>
2022-07-20 22:17:31 +0900

    Step1 모듈 분리 (#66)
    * 도메인 모듈 추가, 의존성 버전 네이밍 적용, 여러 모듈에서 공통적으로 쓰이는 의존성들 base-module 에 정의하고 사용하도록 수정
    
    * domain 모듈 분리
    
    * 빌드 실패 수정 및 잘못 임포트된 패키지 수정
    
    * static import 로 불필요한 패키지 이름이 추가되지 않도록 수정
    
    Co-authored-by: 박찬준 <chanjun.park@navercorp.com>

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
-import edu.nextstep.camp.calculator.domain.Calculator
-import edu.nextstep.camp.calculator.domain.Expression
-import edu.nextstep.camp.calculator.domain.Operator
+import edu.nextstep.camp.domain.Calculator
+import edu.nextstep.camp.domain.Expression
+import edu.nextstep.camp.domain.Operator.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -59,19 +59,19 @@ class MainActivity : AppCompatActivity() {
             binding.textView.text = expression.toString()
         }
         binding.buttonPlus.setOnClickListener {
-            expression += Operator.Plus
+            expression += Plus
             binding.textView.text = expression.toString()
         }
         binding.buttonMinus.setOnClickListener {
-            expression += Operator.Minus
+            expression += Minus
             binding.textView.text = expression.toString()
         }
         binding.buttonMultiply.setOnClickListener {
-            expression += Operator.Multiply
+            expression += Multiply
             binding.textView.text = expression.toString()
         }
         binding.buttonDivide.setOnClickListener {
-            expression += Operator.Divide
+            expression += Divide
             binding.textView.text = expression.toString()
         }
         binding.buttonDelete.setOnClickListener {
