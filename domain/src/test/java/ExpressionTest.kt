b6d3a228337e4080b3ae859f95633008ca3e2b22
SeHyeon Kim <tpgus123123@naver.com>
2022-07-22 20:10:10 +0900

    feat : 모듈 분리 (#80)

--- domain/src/test/java/ExpressionTest.kt
+++ domain/src/test/java/ExpressionTest.kt
@@ -1,6 +1,6 @@
-package edu.nextstep.camp.calculator.domain
-
 import com.google.common.truth.Truth.assertThat
+import edu.nextstep.camp.calculator.domain.Expression
+import edu.nextstep.camp.calculator.domain.Operator
 import org.junit.jupiter.api.Test
 
 class ExpressionTest {
